3a2c22f39b1c99e2d409d56fd242ca6973694daf
Igor <iczaja93@gmail.com>
2017-03-11 18:44:55 +0100

    Added arrow conception

---
+++ BPMNEditor/Shapes/Arrow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BPMNEditor.Shapes
+{
+    /// <summary>
+    /// Interaction logic for Arrow.xaml
+    /// </summary>
+    public partial class Arrow : UserControl
+    {
+
+        public static readonly DependencyProperty StartPointProperty = DependencyProperty.Register(
+            "StartPoint", typeof(Point), typeof(Arrow), new PropertyMetadata(default(Point)));
+
+        public Point StartPoint
+        {
+            get { return (Point) GetValue(StartPointProperty); }
+            set { SetValue(StartPointProperty, value); }
+        }
+
+        public static readonly DependencyProperty EndPointProperty = DependencyProperty.Register(
+            "EndPoint", typeof(Point), typeof(Arrow), new PropertyMetadata(default(Point)));
+
+        public Point EndPoint
+        {
+            get { return (Point) GetValue(EndPointProperty); }
+            set { SetValue(EndPointProperty, value); }
+        }
+
+        public Arrow()
+        {
+            InitializeComponent();
+        }
+    }
+}
