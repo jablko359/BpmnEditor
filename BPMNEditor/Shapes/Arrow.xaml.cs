335b70de1d4c554123f5290a18b6f1a94442dcb1
Igor <iczaja93@gmail.com>
2017-03-12 13:12:35 +0100

    Arrows added

--- BPMNEditor/Shapes/Arrow.xaml.cs
+++ BPMNEditor/Shapes/Arrow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,21 +14,22 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BPMNEditor.Annotations;
 
 namespace BPMNEditor.Shapes
 {
     /// <summary>
     /// Interaction logic for Arrow.xaml
     /// </summary>
-    public partial class Arrow : UserControl
+    public partial class Arrow : UserControl, INotifyPropertyChanged
     {
-
+        #region Dependency properties
         public static readonly DependencyProperty StartPointProperty = DependencyProperty.Register(
             "StartPoint", typeof(Point), typeof(Arrow), new PropertyMetadata(default(Point)));
 
         public Point StartPoint
         {
-            get { return (Point) GetValue(StartPointProperty); }
+            get { return (Point)GetValue(StartPointProperty); }
             set { SetValue(StartPointProperty, value); }
         }
 
@@ -35,13 +38,80 @@ namespace BPMNEditor.Shapes
 
         public Point EndPoint
         {
-            get { return (Point) GetValue(EndPointProperty); }
+            get { return (Point)GetValue(EndPointProperty); }
             set { SetValue(EndPointProperty, value); }
         }
+
+        public static readonly DependencyProperty HeadWidthProperty = DependencyProperty.Register(
+            "HeadWidth", typeof(double), typeof(Arrow), new PropertyMetadata(default(double)));
+
+        public double HeadWidth
+        {
+            get { return (double)GetValue(HeadWidthProperty); }
+            set
+            {
+                SetValue(HeadWidthProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty HeadHeightProperty = DependencyProperty.Register(
+            "HeadHeight", typeof(double), typeof(Arrow), new PropertyMetadata(default(double)));
+
+        public double HeadHeight
+        {
+            get { return (double)GetValue(HeadHeightProperty); }
+            set
+            {
+                SetValue(HeadHeightProperty, value);
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        #endregion
+
+        public Point HeadPoint1 { get; set; }
+        public Point HeadPoint2 { get; set; }
 
         public Arrow()
         {
             InitializeComponent();
+            this.LayoutUpdated += Arrow_LayoutUpdated;
         }
+
+        private void Arrow_LayoutUpdated(object sender, EventArgs e)
+        {
+            CalcualteArrow();
+        }
+
+        private void CalcualteArrow()
+        {
+            double theta = Math.Atan2(StartPoint.Y - EndPoint.Y, StartPoint.X - EndPoint.X);
+            double sint = Math.Sin(theta);
+            double cost = Math.Cos(theta);
+
+            HeadPoint1 = new Point(
+                EndPoint.X + (HeadWidth * cost - HeadHeight * sint),
+                EndPoint.Y + (HeadWidth * sint + HeadHeight * cost));
+
+            HeadPoint2 = new Point(
+                EndPoint.X + (HeadWidth * cost + HeadHeight * sint),
+                EndPoint.Y - (HeadHeight * cost - HeadWidth * sint));
+
+            OnPropertyChanged(nameof(HeadPoint1));
+            OnPropertyChanged(nameof(HeadPoint2));
+        }
+
+
     }
 }
