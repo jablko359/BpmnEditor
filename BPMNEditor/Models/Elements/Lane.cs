bee96c31bb1d51437fefb4389404a5f24954fbfd
Igor <iczaja93@gmail.com>
2017-03-24 18:11:46 +0100

    Added pools and lanes - not working

---
+++ BPMNEditor/Models/Elements/Lane.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPMNEditor.Tools;
+using BPMNEditor.ViewModels;
+
+namespace BPMNEditor.Models.Elements
+{
+    [ElementViewModel(typeof(LaneViewModel), EventViewModel.InitialWidth, EventViewModel.InitialWidth)]
+    public class Lane : IBaseElement
+    {
+    }
+}
---
+++ BPMNEditor/ViewModels/PoolViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPMNEditor.Models.Elements;
+
+namespace BPMNEditor.ViewModels
+{
+    public class PoolViewModel : BaseElementViewModel
+    {
+        public const double InitialWidth = 700;
+        public const double InitialHeight = 350;
+
+        private Pool _pool;
+
+        public PoolViewModel(DocumentViewModel documentViewModel) : base(documentViewModel)
+        {
+        }
+
+        protected override HashSet<Type> ApplicableTypes { get; }
+        protected override IBaseElement CreateElement()
+        {
+            _pool = new Pool();
+            return _pool;
+        }
+    }
+}
