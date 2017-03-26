356b03418d8acb7b790930d9be1ccc870c278d7f
Igor <iczaja93@gmail.com>
2017-03-26 23:27:18 +0200

    Added basic lanes view

--- BPMNEditor/ViewModels/LaneViewModel.cs
+++ BPMNEditor/ViewModels/LaneViewModel.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BPMNEditor.Models.Elements;
 
 namespace BPMNEditor.ViewModels
 {
-    public class LaneViewModel
+    public class LaneViewModel : PropertyChangedBase
     {
+        public const double MinHeight = 75;
+
+        public Lane Lane { get;  }
+        public int Index { get; private set; }
+
+        private double _height;
+        private string _name;
+        private readonly PoolViewModel _pool;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(nameof(Name));
+            }
+        }
+
+
+        public double Height
+        {
+            get { return _height; }
+            set
+            {
+                _height = value;
+                NotifyOfPropertyChange(nameof(Height));
+            }
+        }
+
+
+        public LaneViewModel(int index, PoolViewModel poolViewModel)
+        {
+            Index = index;
+            _name = "Lane";
+            Height = 150;
+            Lane = new Lane();
+            _pool = poolViewModel;
+        }
+
+        public void PoolPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Height")
+            {
+                double lanesHeight = _pool.Lanes.Where(lane => lane != this).Sum(lane => lane.Height);
+                double desiredHeight = _pool.Height - lanesHeight;
+                Height = desiredHeight;
+            }
+        }
     }
 }
--- BPMNEditor/ViewModels/PoolViewModel.cs
+++ BPMNEditor/ViewModels/PoolViewModel.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BPMNEditor.Models.Elements;
+using BPMNEditor.Tools.DragAndDrop;
 
 namespace BPMNEditor.ViewModels
 {
-    public class PoolViewModel : BaseElementViewModel
+    public class PoolViewModel : BaseElementViewModel, IDropable
     {
         public const double InitialWidth = 700;
         public const double InitialHeight = 350;
@@ -15,6 +17,22 @@ namespace BPMNEditor.ViewModels
         private Pool _pool;
 
         private string _name;
+
+        private bool _isDragOver;
+
+        public ObservableCollection<LaneViewModel> Lanes { get; } = new ObservableCollection<LaneViewModel>();
+
+
+        public bool IsDragOver
+        {
+            get { return _isDragOver; }
+            set
+            {
+                _isDragOver = value;
+                NotifyOfPropertyChange(nameof(IsDragOver));
+            }
+        }
+
 
         public string Name
         {
@@ -33,11 +51,65 @@ namespace BPMNEditor.ViewModels
             _name = "Pool";
         }
 
+
         protected override HashSet<Type> ApplicableTypes { get; }
         protected override IBaseElement CreateElement()
         {
             _pool = new Pool();
             return _pool;
         }
+
+        #region IDropable
+
+        public Type DataType => typeof(IPoolElement);
+
+        public void Drop(object data, double x = 0, double y = 0)
+        {
+            IsDragOver = false;
+            ITypeProvider provider = data as ITypeProvider;
+            if (provider != null && provider.ElementType == typeof(Lane))
+            {
+                AddNewLane();
+            }
+        }
+
+        public void DragOver(double x, double y, object dragItem)
+        {
+            IsDragOver = true;
+        }
+
+        public void DragLeave()
+        {
+            IsDragOver = false;
+        }
+
+        #endregion
+
+        private void AddNewLane()
+        {
+            int index = Lanes.Count;
+            LaneViewModel laneViewModel = new LaneViewModel(index, this);
+            PropertyChanged += laneViewModel.PoolPropertyChanged;
+            foreach (LaneViewModel lane in Lanes)
+            {
+                PropertyChanged -= lane.PoolPropertyChanged;
+            }
+            MinHeight = CalculateMinHeight(laneViewModel);
+            if (index == 0)
+            {
+                laneViewModel.Height = Height;
+            }
+            else
+            {
+                Height += laneViewModel.Height;
+            }
+            Lanes.Add(laneViewModel);
+        }
+
+        private double CalculateMinHeight(LaneViewModel addedLane)
+        {
+            return Lanes.Sum(item => item.Height) + LaneViewModel.MinHeight;
+        }
+
     }
 }
